a2c62fdc9d72307822b0068d39821108a24c13eb
Fernando Araujo <araujgom@gmail.com>
2025-02-02 00:23:34 -0300

    refactor: improve code blocks reading and add README file

---
+++ README.md
@@ -0,0 +1,11 @@
+# ePub as MDBooks
+
+I consider myself as a "book hacker". I love to read on different formats and media, and something I catch myself exploring new typografies and layouts for my books. Generally speaking I prefer to read on my Kindle, but technical books tend to be an exception as they contain more complex structures and images that are not well rendered on e-ink screens.
+
+At the same time, I was felt like there was a gap between the simplicity of current standards we see on Markdown-related documentation tools and the complexity of ePub. One great example is mdBook, a tool that allows you to create beautiful documentation websites and/or books from Markdown files. I'd like to have an opportunity to read my technical books through this format instead of the traditional ePub.
+
+So, that's why I decided to create this project. I want to create a tool that can convert an ePub file to a mdBook project. This way, I can read my technical books on a beautiful website, with a great layout and typography, and still have the ability to navigate through the chapters and sections. I also plan to incorporate new features and provide better support.
+
+I hope you enjoy!
+
+**Disclaimer**: This project was created for personal use and don't support piracy in any sense, as it's intended to be used with ePub that you own and have the proper rights to read.
--- epub_parser/converter.py
+++ epub_parser/converter.py
@@ -1,12 +1,12 @@
 import os
-import re
 import shutil
 import zipfile
-from urllib.parse import unquote
+from html import unescape
 import xml.etree.ElementTree as ET
+from urllib.parse import unquote
 
 from bs4 import BeautifulSoup
-import html2text
+from markdownify import markdownify as md
 from slugify import slugify
 
 from .utils import ensure_directory, parse_container
@@ -29,51 +29,44 @@ class EpubConverter:
             zip_ref.extractall(self.temp_dir)
 
     def _parse_opf(self):
-      opf_path = parse_container(self.temp_dir)
-      self.opf_dir = os.path.dirname(os.path.join(self.temp_dir, opf_path))
+        opf_path = parse_container(self.temp_dir)
+        self.opf_dir = os.path.dirname(os.path.join(self.temp_dir, opf_path))
 
-      tree = ET.parse(os.path.join(self.temp_dir, opf_path))
-      ns = {
-          'opf': 'http://www.idpf.org/2007/opf',
-          'dc': 'http://purl.org/dc/elements/1.1/'
-      }
+        tree = ET.parse(os.path.join(self.temp_dir, opf_path))
+        ns = {
+            'opf': 'http://www.idpf.org/2007/opf',
+            'dc': 'http://purl.org/dc/elements/1.1/'
+        }
 
-      # Initialize default values
-      self.title = "Untitled Book"
-      self.author = "Unknown Author"
-      self.manifest = {}
-      self.spine_order = []
+        self.title = "Untitled Book"
+        self.author = "Unknown Author"
+        self.manifest = {}
+        self.spine_order = []
 
-      # Metadata parsing
-      metadata = tree.find('.//opf:metadata', ns)
-      if metadata is not None:
-          # Title parsing
-          title_elem = metadata.find('dc:title', ns) or metadata.find('opf:title', ns)
-          if title_elem is not None and title_elem.text:
-              self.title = title_elem.text.strip()
+        metadata = tree.find('.//opf:metadata', ns)
+        if metadata is not None:
+            title_elem = metadata.find('dc:title', ns) or metadata.find('opf:title', ns)
+            if title_elem is not None and title_elem.text:
+                self.title = title_elem.text.strip()
+            creator_elem = metadata.find('dc:creator', ns) or metadata.find('opf:creator', ns)
+            if creator_elem is not None and creator_elem.text:
+                self.author = creator_elem.text.strip()
 
-          # Author parsing
-          creator_elem = metadata.find('dc:creator', ns) or metadata.find('opf:creator', ns)
-          if creator_elem is not None and creator_elem.text:
-              self.author = creator_elem.text.strip()
+        manifest = tree.find('.//opf:manifest', ns)
+        if manifest is not None:
+            for item in manifest.findall('opf:item', ns):
+                self.manifest[item.attrib['id']] = {
+                    'href': unquote(item.attrib['href']),
+                    'media_type': item.attrib['media-type']
+                }
 
-      # Manifest parsing
-      manifest = tree.find('.//opf:manifest', ns)
-      if manifest is not None:
-          for item in manifest.findall('opf:item', ns):
-              self.manifest[item.attrib['id']] = {
-                  'href': unquote(item.attrib['href']),
-                  'media_type': item.attrib['media-type']
-              }
-
-      # Spine parsing
-      spine = tree.find('.//opf:spine', ns)
-      if spine is not None:
-          self.spine_order = [
-              itemref.attrib['idref']
-              for itemref in spine.findall('opf:itemref', ns)
-              if 'idref' in itemref.attrib
-          ]
+        spine = tree.find('.//opf:spine', ns)
+        if spine is not None:
+            self.spine_order = [
+                itemref.attrib['idref']
+                for itemref in spine.findall('opf:itemref', ns)
+                if 'idref' in itemref.attrib
+            ]
 
     def _process_images(self, soup, xhtml_path):
         for img in soup.find_all('img'):
@@ -82,7 +75,6 @@ class EpubConverter:
                 os.path.dirname(xhtml_path),
                 unquote(src)
             ))
-
             if os.path.exists(abs_path):
                 rel_path = os.path.relpath(abs_path, self.temp_dir)
                 target_dir = os.path.join(self.assets_dir, os.path.dirname(rel_path))
@@ -90,24 +82,9 @@ class EpubConverter:
                 shutil.copy(abs_path, os.path.join(target_dir, os.path.basename(rel_path)))
                 img['src'] = os.path.join('assets', rel_path)
 
-    def _process_code_blocks(self, soup):
-        for pre in soup.find_all('pre'):
-            code = pre.find('code')
-            if code:
-                lang = ''
-                if code.has_attr('class'):
-                    for cls in code['class']:
-                        if cls.startswith('language-'):
-                            lang = cls.split('-', 1)[1]
-                            break
-                code_content = code.get_text()
-                pre.replace_with(f'\n```{lang}\n{code_content}\n```\n')
-
     def convert_to_md(self):
         self._extract_epub()
         self._parse_opf()
-        h = html2text.HTML2Text()
-        h.body_width = 0
 
         for item_id in self.spine_order:
             item = self.manifest[item_id]
@@ -119,19 +96,18 @@ class EpubConverter:
                 soup = BeautifulSoup(f, 'html.parser')
 
             self._process_images(soup, xhtml_path)
-            self._process_code_blocks(soup)
 
-            # Convert to Markdown
-            md_content = h.handle(str(soup))
+            md_content = md(str(soup), heading_style="ATX")
             md_filename = f"{slugify(os.path.basename(item['href']))}.md"
             md_path = os.path.join(self.md_dir, md_filename)
 
             with open(md_path, 'w', encoding='utf-8') as f:
-                f.write(f"# {soup.title.string}\n\n" if soup.title else "")
+                if soup.title and soup.title.string:
+                    f.write(f"# {soup.title.string}\n\n")
                 f.write(md_content)
 
             self.toc.append({
-                'title': soup.title.string if soup.title else md_filename,
+                'title': soup.title.string if (soup.title and soup.title.string) else md_filename,
                 'path': md_filename
             })
 
@@ -143,7 +119,6 @@ class EpubConverter:
         summary = "# Summary\n\n"
         for entry in self.toc:
             summary += f"* [{entry['title']}]({entry['path']})\n"
-
         with open(os.path.join(self.md_dir, 'SUMMARY.md'), 'w') as f:
             f.write(summary)
 
