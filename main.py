992dbf29adbbb953ecd3581e4d132ed70d4b4bef
Fernando Araujo <araujgom@gmail.com>
2025-02-02 18:38:19 -0300

    feat: improve main function

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
 import sys
+import subprocess
 from epub_parser.converter import EpubConverter
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print("Usage: python main.py <input.epub> <output_dir>")
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <input.epub> [output_dir]")
         sys.exit(1)
 
-    converter = EpubConverter(sys.argv[1], sys.argv[2])
+    epub_path = sys.argv[1]
+    output_dir = sys.argv[2] if len(sys.argv) > 2 else "output/"
+
+    converter = EpubConverter(epub_path, output_dir)
     converter.convert_to_md()
-    print(f"mdBook project created at {sys.argv[2]}")
+    print(f"mdBook project created at {output_dir}")
+
+    # Build the mdBook project.
+    subprocess.run(["mdbook", "build"], cwd=output_dir, check=True)
+    print("mdBook project built.")
+
+    # Serve the mdBook project.
+    subprocess.run(["mdbook", "serve"], cwd=output_dir, check=True)
